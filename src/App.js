f50a8d12c52072f7f94458884524955902b11b58
QueenVivi <hi@vivianxu.com>
2022-09-20 23:48:30 +1000

    ✨ Added Github repo url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        <a
+          href="https://github.com/QueenVivi/react-weather-application"
+          title="github page for the code source"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source
+        </a>{" "}
+        coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/vivian-ux/"
+          title="Vivian's linkedin page"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Vivian Xu
+        </a>
+      </footer>
+    </div>
+  );
 }
