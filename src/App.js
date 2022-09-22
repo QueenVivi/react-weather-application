ea9cd7e4c95354b703b5d08a36b88e277f28020c
QueenVivi <hi@vivianxu.com>
2022-09-22 17:48:39 +1000

    ✨ Added Search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Search from "./Search";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Search />
         <footer>
           <a
             href="https://github.com/QueenVivi/react-weather-application"
