156969b0684db1d90e19f1a4f3299426ea7e3898
QueenVivi <hi@vivianxu.com>
2022-09-22 10:03:14 +1000

    ✨ Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        <a
-          href="https://github.com/QueenVivi/react-weather-application"
-          title="github page for the code source"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source
-        </a>{" "}
-        coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/vivian-ux/"
-          title="Vivian's linkedin page"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Vivian Xu
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/QueenVivi/react-weather-application"
+            title="github page for the code source"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source
+          </a>{" "}
+          coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/vivian-ux/"
+            title="Vivian's linkedin page"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Vivian Xu
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
